8f341f184bf6e336bbf968273b4337a4c4d141c6
Mark Ian Pamintuan <ianpamintuan06@gmail.com>
2021-05-07 21:07:09 +0800

    refactor: Refactor Button component

--- common/Variety.ts
+++ common/Variety.ts
@@ -1,13 +1,13 @@
 export enum ButtonVariety {
-    Primary = "bg-blue-500 hover:bg-blue-700 text-white",
-    Danger = "bg-red-500 hover:bg-red-700 text-white",
-    Secondary = "bg-gray-500 hover:bg-gray-700 text-white",
-    Warning = "bg-yellow-500 hover:bg-yellow-700 text-white",
-    Info = "bg-blue-400 hover:bg-blue-600 text-white",
+	Primary = "bg-blue-500 hover:bg-blue-700",
+	Danger = "bg-red-500 hover:bg-red-700",
+	Secondary = "bg-gray-500 hover:bg-gray-700",
+	Warning = "bg-yellow-500 hover:bg-yellow-700",
+	Info = "bg-blue-400 hover:bg-blue-600",
 }
 
 export enum ButtonSize {
-    Small = "py-1 px-2",
-    Default = "py-2 px-4",
-    Large = "py-4 px-8"
+	Small = "py-1 px-6",
+	Default = "py-2 px-9",
+	Large = "py-4 px-12",
 }
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,31 +3,37 @@ import { ButtonSize, ButtonVariety } from "../../common/Variety";
 import classNames from "classnames";
 
 export interface ButtonProps {
-    className?: string;
-    variety?: ButtonVariety;
-    size?: ButtonSize;
-    disabled?: boolean;
-    onClick?: MouseEventHandler<HTMLButtonElement>;
-    children: ReactNode;
+	className?: string;
+	variety?: ButtonVariety;
+	size?: ButtonSize;
+	disabled?: boolean;
+	onClick?: MouseEventHandler<HTMLButtonElement>;
+	children: ReactNode;
 }
 
 export const Button: FunctionComponent<ButtonProps> = ({
-    className,
-    variety = ButtonVariety.Primary,
-    size = ButtonSize.Default,
-    disabled = false,
-    children,
-    ...props
+	className,
+	variety = ButtonVariety.Primary,
+	size = ButtonSize.Default,
+	disabled = false,
+	children,
+	...props
 }) => {
-    return (
-        <button
-            className={classNames("rounded", className, variety, size, {
-                "opacity-50 cursor-not-allowed": disabled,
-            })}
-            disabled={disabled}
-            {...props}
-        >
-            {children}
-        </button>
-    );
+	return (
+		<button
+			className={classNames(
+				"rounded text-white",
+				className,
+				variety,
+				size,
+				{
+					"opacity-50 cursor-not-allowed": disabled,
+				}
+			)}
+			disabled={disabled}
+			{...props}
+		>
+			{children}
+		</button>
+	);
 };
